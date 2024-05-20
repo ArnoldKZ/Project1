a7c0cf9dcf66015e5f5d318a2ac9cbd77f5b4851
Ashton <arnoldkz2000@yahoo.com>
2024-05-20 13:56:09 -0400

    added a function

--- first.sh
+++ first.sh
@@ -1,6 +1,7 @@
 #!/bin/#!/usr/bin/env bash
 # Script that tells weather you are older or younger than 30 years
 #this will be a good program
+adult(){
 echo "Please enter your age"
 read age
 if [[ $age -eq 30 ]]; then
@@ -8,3 +9,5 @@ if [[ $age -eq 30 ]]; then
 else
   echo "You are old"
 fi
+}
+adult
