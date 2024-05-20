1e5ea4aa21949e8ee3de2340c9a6235fe4cb87cf
Ashton <arnoldkz2000@yahoo.com>
2024-05-20 10:44:58 -0400

    Added a comment

--- first.sh
+++ first.sh
@@ -1,6 +1,6 @@
 #!/bin/#!/usr/bin/env bash
 # Script that tells weather you are older or younger than 30 years
-
+#this will be a good program
 echo "Please enter your age"
 read age
 if [[ $age -eq 30 ]]; then
